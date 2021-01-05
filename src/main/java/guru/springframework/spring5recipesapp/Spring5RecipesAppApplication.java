2e1f7c294bc0094ddcc011ebcbb46c252b593683
amritoV <verrelliamrito94@gmail.com>
2021-01-05 14:27:51 +0100

    Adding recipe web project

---
+++ src/main/java/guru/springframework/spring5recipesapp/Spring5RecipesAppApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5recipesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5RecipesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5RecipesAppApplication.class, args);
+	}
+
+}
